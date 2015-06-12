33bfe962ec9e9df47564dc07fcb1adad81c33eaa
Václav Makeš <info@makes.cz>
2015-06-12 23:41:22 +0200

    Added functions for convert input URI to output URI + tests

--- src/functions.php
+++ src/functions.php
@@ -5,3 +5,20 @@ function getServiceNameFromUri($uri)
 	preg_match('/^\/([a-z]+)\/.*/', $uri, $match);
 	return $match[1];
 }
+
+
+function getEndOfPath($uri)
+{
+	preg_match('/^\/[a-z]+\/(.*)/', $uri, $match);
+	return $match[1];
+}
+
+
+function convertUri($inputUri)
+{
+	$outputUri = 'http://';
+	$outputUri .= getServiceNameFromUri($inputUri);
+	$outputUri .= '.services.procvic.cz/';
+	$outputUri .= getEndOfPath($inputUri);
+	return $outputUri;
+}
--- test/functions.php
+++ test/functions.php
@@ -3,8 +3,12 @@
 include __DIR__ . '/../src/functions.php';
 
 assert_options(ASSERT_ACTIVE, 1);
-assert_options(ASSERT_WARNING, 0);
-assert_options(ASSERT_QUIET_EVAL, 1);
 
 assert(getServiceNameFromUri('/categories/subjects') === 'categories');
 assert(getServiceNameFromUri('/user/get-info/10') === 'user');
+
+assert(getEndOfPath('/categories/subjects') === 'subjects');
+assert(getEndOfPath('/user/get-info/10') === 'get-info/10');
+
+assert(convertUri('/categories/subjects') === 'http://categories.services.procvic.cz/subjects');
+assert(convertUri('/user/get-info/10') === 'http://user.services.procvic.cz/get-info/10');
