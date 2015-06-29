5571f9f8d3f00473630a29869af53d319546062f
Václav Makeš <info@makes.cz>
2015-06-29 10:55:00 +0200

    Added support for check authorize all input requests

--- src/index.php
+++ src/index.php
@@ -4,5 +4,17 @@ include __DIR__ . "/functions.php";
 
 $oldUri = $_SERVER['REQUEST_URI'];
 $newUri = convertUri($oldUri);
+$userToken = getTokenFromUri($oldUri);
+$urlForAuthorizeUser = 'auth.procvic.cz/lockdin/check-authorize?access_token=' . $userToken;
+
+if (mustCheckTokenForUrl($oldUri)) {
+	$userValidJson = (string) readContentFromUrl($urlForAuthorizeUser);
+	$isUserLogIn = isValidUser($userValidJson);
+	if (!$isUserLogIn) {
+		renderAsJSON('{"error":"Unauthorize request."}');
+		die;
+	}
+}
+
 $content = (string) readContentFromUrl($newUri);
 renderAsJSON($content);
