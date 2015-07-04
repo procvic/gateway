3e485f0664cf048ca98fd3b8536e99f4e14b6057
Václav Makeš <info@makes.cz>
2015-07-04 23:13:58 +0200

    Fix - new address for authorize user

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@ include __DIR__ . "/functions.php";
 $oldUri = $_SERVER['REQUEST_URI'];
 $newUri = convertUri($oldUri);
 $userToken = getTokenFromUri($oldUri);
-$urlForAuthorizeUser = 'auth.procvic.cz/lockdin/check-authorize?access_token=' . $userToken;
+$urlForAuthorizeUser = 'auth.procvic.cz/authorize?access_token=' . $userToken;
 
 if (mustCheckTokenForUrl($oldUri)) {
 	$userValidJson = (string) readContentFromUrl($urlForAuthorizeUser);
